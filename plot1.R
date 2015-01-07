c5ef8c24a8f1bcf3ab59a2b704a7f1f20107d7ba
Thomas Thevis <thomas@thevis.net>
2015-01-07 22:45:31 +0100

    added R script and result png plot for first task

---
+++ plot1.R
@@ -0,0 +1,35 @@
+library(data.table)
+
+#
+# Checks if the required data file is present in the working directory
+# and fetches and extracts it from the external URL otherwise
+#
+getDataFile <- function(fileName="household_power_consumption.txt",
+                    fetchURL="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip") {
+        if (!file.exists(fileName)) {
+                download.file(fetchURL, destfile = "tmp.zip", method = "curl")
+                unzip("tmp.zip")
+                file.remove("tmp.zip")
+        }
+        fileName
+}
+
+#
+# Reads the relevant data (two days in Feb. 2007) into a data table with proper
+# table headers
+#
+getFilteredTable <- function(fileName, filterPattern="^[12]/2/2007") {
+        table <- fread(paste("grep", filterPattern, fileName, sep=" "), na.strings="?")
+        header <- readLines(fileName, n=1)
+        cols <- unlist(strsplit(header, ";"))
+        setattr(table, 'names', cols)
+}
+
+input <- getDataFile()
+table <- getFilteredTable(input)
+
+png("plot1.png")
+hist(table$Global_active_power, main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     col="red")
+dev.off()
